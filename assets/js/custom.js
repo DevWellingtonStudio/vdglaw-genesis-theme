80de7ac4818b54884ebc279a604a4a2571b06e6f
steelbridge-io <chris@steelbridge.io>
2020-10-22 17:42:58 -0700

    Added background color to nav

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,10 @@ $('.parallax-window').parallax({
   naturalWidth: 600,
   naturalHeight: 400
 });
+
+$(function () {
+  $(document).scroll(function () {
+	 var $nav = $(".bg-clear");
+	 $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
